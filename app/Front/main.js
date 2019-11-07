09b58361e015f66cc3cf960638ae5a313505edea
Moises <moisesisneros@gmail.com>
2019-11-07 15:51:08 -0500

    Agrego tabla

--- app/Front/main.js
+++ app/Front/main.js
@@ -5,7 +5,6 @@ var main = function () {
     let contador = 1;
 
     this.init = function(data){
-        console.log("Desde init",data);
         var fecha = self.funcionTiempo();
         self.insertarTabla(fecha,data,contador);
         contador += 1;
@@ -17,18 +16,24 @@ var main = function () {
     };
     
     this.insertarTabla = function(fecha,data,contador) {
-        $('#tabla').html(`
-        <tr>
-        <th scope="row">${contador}</th>
-        <td>${fecha}</td>
-        <td>${data.matricula}</td>
-        <td>${data.lab}</td>
-      </tr>`);
+        var table = $('#table').DataTable();
+        
+        table.row.add( [
+            contador,
+            fecha,
+            data.matricula,
+            data.lab
+        ]).draw(false);
     };
 
     this.cambiaTabla = function() {
         $(document).ready(function() {
-            $('#table').DataTable();
+            $('#table').DataTable({
+                dom: 'Bfrtip',
+                buttons: [
+                    'copy', 'csv', 'excel', 'pdf', 'print'
+                ]
+            });
         });
     };
 };
